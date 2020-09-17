47570a249b100dfe921382872f59e58c459db252
Alpha <mavamalonga.alpha@gmail.com>
2020-09-17 03:07:32 +0200

    start create labyrinthe

--- models.py
+++ models.py
@@ -1,50 +1,34 @@
 import pygame
 import sys
+from variables import * #all variables from this file
 pygame.init()
+		
+
+class Labyrinthe():
+	def __init__(self, base, sp_size):
+		pygame.init()
+		self.base_lab = base
+		self.screen = pygame.display.set_mode((15*sp_size, 15*sp_size))
+
+	def init_lab(self):
+		self.structures = []
+		for ln in self.base_lab:
+			line_level = list(ln)
+			self.structures.append(line_level)
+
+		for x, col in enumerate(self.structures):
+			for y, case in enumerate(col):
+				pos_x = x*sp_size
+				pos_y = y*sp_size
+				if case == "m":
+					self.screen.blit(pygame.image.load("ressource/MacGyver.png"), (pos_x, pos_y))
+				elif case == "g":
+					self.screen.blit(pygame.image.load("ressource/Gardien.png"), (pos_x, pos_y))
+				elif case == "s":
+					self.screen.blit(pygame.image.load("ressource/structures.png"), (pos_x, pos_y))
 
 
-
-class Position:
-	def __init__(self, x, y):
-		self.x = x
-		self.y = y
-
-
-class Labyrinthe(pygame.sprite.Sprite):
-	ZONES = []
-	MIN_CASE = 0
-	MAX_CASE = 15
-	WIDTH_CASE = 1
-	MIN_CASEY = 0
-	MAX_CASEY = 15
-	HEIGHT_CASE = 1
-
-	def __init__(self):
-		super().__init__()
-		self.vide = []
-
-
-	@classmethod
-	def initialize_zones(cls):
-		for caseX in range(cls.MIN_CASE, cls.MAX_CASE, cls.WIDTH_CASE):
-			for caseY in range(cls.MIN_CASEY, cls.MAX_CASEY, cls.HEIGHT_CASE):
-				bottom_left_corner = Position(caseX, caseY)
-				top_right_corner = Position(caseX + cls.WIDTH_CASE, caseY + cls.HEIGHT_CASE)
-				zone = Labyrinthe(bottom_left_corner, top_right_corner)
-				cls.ZONES.append(zone)
-				print(len(cls.ZONES))
-
-
-
-
-class MacGyver(pygame.sprite.Sprite):
-	def __init__(self):
-		super().__init__()
-		self.image = pygame.image.load('ressource/MacGyver.png')
-		self.rect = self.image.get_rect()
-		self.rect.x = 100
-		self.rect.y = 100
-		self.position = Position(self.rect.x, self.rect.y)
+		pygame.display.flip()
 
 
 
---
+++ variables.py
@@ -0,0 +1,24 @@
+import pygame
+pygame.init()
+
+
+sp_size = 35
+
+lab = [
+"sssssssssssssss",
+"svgvvvvvvvvvvvs",
+"svvvvvvvvvvvvvs",
+"svvvvvvvvvvvvvs",
+"svvvvvvvvvvvvvs",
+"svvvvvvvvvvvvvs",
+"svvvvvvvvvvvvvs",
+"svvvvvvvvvvvvvs",
+"svvvvvvvvvvvvvs",
+"svvvvvvvvvvvvvs",
+"svvvvvvvvvvvvvs",
+"svvvvvvvvvvvvvs",
+"svvvvvvvvvvvvvs",
+"svvvvvvmvvvvvvs",
+"sssssssssssssss"
+]
+
